87f29e0747834b2ac743e4002a503606e9add124
krsntn <kanahan0110@gmail.com>
2020-02-17 19:02:54 +0800

    add valentine & singles day

--- src/js/calc.js
+++ src/js/calc.js
@@ -1,77 +1,97 @@
 export const calcYear = time => {
-  const today = new Date(time);
-  const firstDate = new Date(new Date().getFullYear(), 0, 1);
-  const lastDate = new Date(new Date().getFullYear(), 11, 31);
+	const today = new Date(time);
+	const firstDate = new Date(new Date().getFullYear(), 0, 1);
+	const lastDate = new Date(new Date().getFullYear(), 11, 31);
 
-  return calcDayDiff(firstDate, today, lastDate);
+	return calcDayDiff(firstDate, today, lastDate);
 };
 
 export const calcQuarter = time => {
-  const today = new Date(time);
-  const firstDate = new Date(new Date().getFullYear(), 0, 1);
+	const today = new Date(time);
+	const firstDate = new Date(new Date().getFullYear(), 0, 1);
 
-  let endMonth = 0;
-  let endDate = 31;
+	let endMonth = 0;
+	let endDate = 31;
 
-  if (today.getMonth() < 3) {
-    endMonth = 2;
-  } else if (today.getMonth() < 6) {
-    endMonth = 5;
-  } else if (today.getMonth() < 9) {
-    endMonth = 8;
-    endDate = 30;
-  } else {
-    endMonth = 11;
-  }
+	if (today.getMonth() < 3) {
+		endMonth = 2;
+	} else if (today.getMonth() < 6) {
+		endMonth = 5;
+	} else if (today.getMonth() < 9) {
+		endMonth = 8;
+		endDate = 30;
+	} else {
+		endMonth = 11;
+	}
 
-  const lastDate = new Date(new Date().getFullYear(), endMonth, endDate);
+	const lastDate = new Date(new Date().getFullYear(), endMonth, endDate);
 
-  return calcDayDiff(firstDate, today, lastDate);
+	return calcDayDiff(firstDate, today, lastDate);
 };
 
 export const calcMonth = time => {
-  const today = new Date(time);
-  const firstDate = new Date(today.getFullYear(), today.getMonth(), 1);
-  const lastDate = new Date(
-    new Date().getFullYear(),
-    new Date().getMonth() + 1,
-    0
-  );
+	const today = new Date(time);
+	const firstDate = new Date(today.getFullYear(), today.getMonth(), 1);
+	const lastDate = new Date(
+		new Date().getFullYear(),
+		new Date().getMonth() + 1,
+		0
+	);
 
-  return calcDayDiff(firstDate, today, lastDate);
+	return calcDayDiff(firstDate, today, lastDate);
 };
 
 export const calcWeek = time => {
-  const now = new Date(time);
-  const firstDate = new Date(
-    new Date().setDate(
-      now.getDate() - now.getDay() + (now.getDay() === 0 ? -6 : 1)
-    )
-  );
-  const lastDate = new Date(new Date().setDate(firstDate.getDate() + 6));
+	const now = new Date(time);
+	const firstDate = new Date(
+		new Date().setDate(
+			now.getDate() - now.getDay() + (now.getDay() === 0 ? -6 : 1)
+		)
+	);
+	const lastDate = new Date(new Date().setDate(firstDate.getDate() + 6));
 
-  return calcDayDiff(firstDate, now, lastDate);
+	return calcDayDiff(firstDate, now, lastDate);
 };
 
 export const calcDay = () => {
-  const start = new Date();
-  start.setHours(0, 0, 0, 0);
+	const start = new Date();
+	start.setHours(0, 0, 0, 0);
 
-  const end = new Date();
-  end.setHours(23, 59, 59, 999);
+	const end = new Date();
+	end.setHours(23, 59, 59, 999);
 
-  const passedDay = Math.abs(new Date() - start);
-  const totalDay = Math.abs(end - start);
+	const passedDay = Math.abs(new Date() - start);
+	const totalDay = Math.abs(end - start);
 
-  return (passedDay / totalDay) * 100;
+	return (passedDay / totalDay) * 100;
 };
 
+export const calcValentine = now => {
+	return calcActualDate(now, '2/14');
+};
+
+export const calc1111 = now => {
+	return calcActualDate(now, '11/11');
+};
+
+function calcActualDate(now, theDate) {
+	const lastYearDate = new Date(`${now.getFullYear() - 1}/${theDate}`);
+	const thisYearDate = new Date(`${now.getFullYear()}/${theDate}`);
+	const nextYearDate = new Date(`${now.getFullYear() + 1}/${theDate}`);
+
+	if (now < thisYearDate) {
+		return calcDayDiff(lastYearDate, now, thisYearDate);
+	}
+
+	return calcDayDiff(thisYearDate, now, nextYearDate);
+}
+
 function calcDayDiff(start, cur, end) {
-  start = start.setHours(0, 0, 0, 0);
-  end = end.setHours(23, 59, 59, 999);
+	start = start.setHours(0, 0, 0, 0);
+	end = end.setHours(23, 59, 59, 999);
 
-  const diffTime = Math.abs(cur - start);
-  const totalDiffInMilliseconds = Math.abs(end - start);
+	const diffTime = Math.abs(cur - start);
+	const totalDiffInMilliseconds = Math.abs(end - start);
 
-  return (diffTime / totalDiffInMilliseconds) * 100;
+	return (diffTime / totalDiffInMilliseconds) * 100;
 }

49b33aa46e39c78d3b6fc404fb50fbbe22cd4edc
krsntn <kanahan0110@gmail.com>
2020-02-18 23:03:39 +0800

    add father & mother's day

--- src/App.jsx
+++ src/App.jsx
@@ -1,109 +1,126 @@
 import React, { useState, useEffect, useCallback, useMemo } from 'react';
 import css from './App.module.scss';
 import {
-	calcYear,
-	calcQuarter,
-	calcMonth,
-	calcWeek,
-	calcDay,
-	calcValentine,
-	calc1111,
+  calcYear,
+  calcQuarter,
+  calcMonth,
+  calcWeek,
+  calcDay,
+  calcValentine,
+  calc1111,
+  calcMonthWeekDay,
 } from './js/calc';
 import Progress from './components/ProgressBar';
 import Switch from './components/Switch';
 
 const App = () => {
-	const [now, setNow] = useState(new Date().toLocaleString());
-	const [yearPercent, setYearPercent] = useState(0);
-	const [quarterPercent, setQuarterPercent] = useState(0);
-	const [monthPercent, setMonthPercent] = useState(0);
-	const [weekPercent, setWeekPercent] = useState(0);
-	const [dayPercent, setDayPercent] = useState(0);
-	const [valentinePercent, setValentinePercent] = useState(0);
-	const [singlesPencent, setSinglesPencent] = useState(0);
-	const [showDecimal, setShowDecimal] = useState(false);
+  const [now, setNow] = useState(new Date().toLocaleString());
+  const [yearPercent, setYearPercent] = useState(0);
+  const [quarterPercent, setQuarterPercent] = useState(0);
+  const [monthPercent, setMonthPercent] = useState(0);
+  const [weekPercent, setWeekPercent] = useState(0);
+  const [dayPercent, setDayPercent] = useState(0);
+  const [valentinePercent, setValentinePercent] = useState(0);
+  const [singlesPercent, setSinglesPercent] = useState(0);
+  const [fathersDayPercent, setFathersDayPercent] = useState(0);
+  const [mothersDayPercent, setMothersDayPercent] = useState(0);
+  const [showDecimal, setShowDecimal] = useState(false);
 
-	useEffect(() => {
-		setInterval(() => {
-			const newTime = new Date();
-			setNow(newTime.toLocaleString());
-			setYearPercent(calcYear(newTime));
-			setQuarterPercent(calcQuarter(newTime));
-			setMonthPercent(calcMonth(newTime));
-			setWeekPercent(calcWeek(newTime));
-			setDayPercent(calcDay());
-			setValentinePercent(calcValentine(newTime));
-			setSinglesPencent(calc1111(newTime));
-		}, 1000);
-	}, []);
+  useEffect(() => {
+    setInterval(() => {
+      const newTime = new Date();
+      setNow(newTime.toLocaleString());
+      setYearPercent(calcYear(newTime));
+      setQuarterPercent(calcQuarter(newTime));
+      setMonthPercent(calcMonth(newTime));
+      setWeekPercent(calcWeek(newTime));
+      setDayPercent(calcDay());
+      setValentinePercent(calcValentine(newTime));
+      setSinglesPercent(calc1111(newTime));
+      setFathersDayPercent(calcMonthWeekDay(newTime, 6, 3, 7));
+      setMothersDayPercent(calcMonthWeekDay(newTime, 5, 2, 7));
+    }, 1000);
+  }, []);
 
-	const toggleSwitch = useCallback(() => {
-		setShowDecimal(!showDecimal);
-	}, [showDecimal]);
+  const toggleSwitch = useCallback(() => {
+    setShowDecimal(!showDecimal);
+  }, [showDecimal]);
 
-	const timeValue = useMemo(() => {
-		return {
-			yearValue: formatValue(showDecimal, yearPercent),
-			quarterValue: formatValue(showDecimal, quarterPercent),
-			monthValue: formatValue(showDecimal, monthPercent),
-			weekValue: formatValue(showDecimal, weekPercent),
-			dayValue: formatValue(showDecimal, dayPercent),
-			valentineValue: formatValue(showDecimal, valentinePercent),
-			singlesValue: formatValue(showDecimal, singlesPencent),
-		};
-	}, [
-		showDecimal,
-		yearPercent,
-		quarterPercent,
-		monthPercent,
-		weekPercent,
-		dayPercent,
-		valentinePercent,
-		singlesPencent,
-	]);
+  const timeValue = useMemo(() => {
+    return {
+      yearValue: formatValue(showDecimal, yearPercent),
+      quarterValue: formatValue(showDecimal, quarterPercent),
+      monthValue: formatValue(showDecimal, monthPercent),
+      weekValue: formatValue(showDecimal, weekPercent),
+      dayValue: formatValue(showDecimal, dayPercent),
+      valentineValue: formatValue(showDecimal, valentinePercent),
+      singlesValue: formatValue(showDecimal, singlesPercent),
+      fathersDayValue: formatValue(showDecimal, fathersDayPercent),
+      mothersDayValue: formatValue(showDecimal, mothersDayPercent),
+    };
+  }, [
+    showDecimal,
+    yearPercent,
+    quarterPercent,
+    monthPercent,
+    weekPercent,
+    dayPercent,
+    valentinePercent,
+    singlesPercent,
+    fathersDayPercent,
+    mothersDayPercent,
+  ]);
 
-	return (
-		<div className={css.App}>
-			<div className="container">
-				<div className={css.box}>
-					<div className={css.titleDiv}>
-						<div className={css.bigTitle}>Progress</div>
-						<Switch isChecked={showDecimal} toggleSwitch={toggleSwitch} />
-					</div>
-					<div className={css.bigDescription}>{now}</div>
+  return (
+    <div className={css.App}>
+      <div className="container">
+        <div className={css.box}>
+          <div className={css.titleDiv}>
+            <div className={css.bigTitle}>Progress</div>
+            <Switch isChecked={showDecimal} toggleSwitch={toggleSwitch} />
+          </div>
+          <div className={css.bigDescription}>{now}</div>
 
-					<Progress title="Year" percentage={timeValue.yearValue}></Progress>
-					<Progress
-						title="Quarter"
-						percentage={timeValue.quarterValue}
-					></Progress>
-					<Progress title="Month" percentage={timeValue.monthValue}></Progress>
-					<Progress title="Week" percentage={timeValue.weekValue}></Progress>
-					<Progress title="Today" percentage={timeValue.dayValue}></Progress>
-					<Progress
-						title="Next Valentine's Day"
-						percentage={timeValue.valentineValue}
-					></Progress>
-					<Progress
-						title="Singles' Day"
-						percentage={timeValue.singlesValue}
-					></Progress>
+          <Progress title="Year" percentage={timeValue.yearValue}></Progress>
+          <Progress
+            title="Quarter"
+            percentage={timeValue.quarterValue}
+          ></Progress>
+          <Progress title="Month" percentage={timeValue.monthValue}></Progress>
+          <Progress title="Week" percentage={timeValue.weekValue}></Progress>
+          <Progress title="Today" percentage={timeValue.dayValue}></Progress>
+          <Progress
+            title="Next Valentine's Day"
+            percentage={timeValue.valentineValue}
+          ></Progress>
+          <Progress
+            title="Singles' Day"
+            percentage={timeValue.singlesValue}
+          ></Progress>
+          <Progress
+            title="Father's Day"
+            percentage={timeValue.fathersDayValue}
+          ></Progress>
+          <Progress
+            title="Mother's Day"
+            percentage={timeValue.mothersDayValue}
+          ></Progress>
 
-					<div></div>
-					<div className={css.footer}>
-						<div>The only progress bar you'd rather see go slower.</div>
-						<div>
-							Made by <a href="https://github.com/krsntn">Karson.</a>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+          <div></div>
+          <div className={css.footer}>
+            <div>The only progress bar you'd rather see go slower.</div>
+            <div>
+              Made by <a href="https://github.com/krsntn">Karson.</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 const formatValue = (showDecimal, value) => {
-	return showDecimal ? value.toFixed(10) : Math.floor(value);
+  return showDecimal ? value.toFixed(10) : Math.floor(value);
 };
 
 export default App;
--- src/js/calc.js
+++ src/js/calc.js
@@ -1,97 +1,134 @@
 export const calcYear = time => {
-	const today = new Date(time);
-	const firstDate = new Date(new Date().getFullYear(), 0, 1);
-	const lastDate = new Date(new Date().getFullYear(), 11, 31);
+  const today = new Date(time);
+  const firstDate = new Date(new Date().getFullYear(), 0, 1);
+  const lastDate = new Date(new Date().getFullYear(), 11, 31);
 
-	return calcDayDiff(firstDate, today, lastDate);
+  return calcDayDiff(firstDate, today, lastDate);
 };
 
 export const calcQuarter = time => {
-	const today = new Date(time);
-	const firstDate = new Date(new Date().getFullYear(), 0, 1);
+  const today = new Date(time);
+  const firstDate = new Date(new Date().getFullYear(), 0, 1);
 
-	let endMonth = 0;
-	let endDate = 31;
+  let endMonth = 0;
+  let endDate = 31;
 
-	if (today.getMonth() < 3) {
-		endMonth = 2;
-	} else if (today.getMonth() < 6) {
-		endMonth = 5;
-	} else if (today.getMonth() < 9) {
-		endMonth = 8;
-		endDate = 30;
-	} else {
-		endMonth = 11;
-	}
+  if (today.getMonth() < 3) {
+    endMonth = 2;
+  } else if (today.getMonth() < 6) {
+    endMonth = 5;
+  } else if (today.getMonth() < 9) {
+    endMonth = 8;
+    endDate = 30;
+  } else {
+    endMonth = 11;
+  }
 
-	const lastDate = new Date(new Date().getFullYear(), endMonth, endDate);
+  const lastDate = new Date(new Date().getFullYear(), endMonth, endDate);
 
-	return calcDayDiff(firstDate, today, lastDate);
+  return calcDayDiff(firstDate, today, lastDate);
 };
 
 export const calcMonth = time => {
-	const today = new Date(time);
-	const firstDate = new Date(today.getFullYear(), today.getMonth(), 1);
-	const lastDate = new Date(
-		new Date().getFullYear(),
-		new Date().getMonth() + 1,
-		0
-	);
+  const today = new Date(time);
+  const firstDate = new Date(today.getFullYear(), today.getMonth(), 1);
+  const lastDate = new Date(
+    new Date().getFullYear(),
+    new Date().getMonth() + 1,
+    0
+  );
 
-	return calcDayDiff(firstDate, today, lastDate);
+  return calcDayDiff(firstDate, today, lastDate);
 };
 
 export const calcWeek = time => {
-	const now = new Date(time);
-	const firstDate = new Date(
-		new Date().setDate(
-			now.getDate() - now.getDay() + (now.getDay() === 0 ? -6 : 1)
-		)
-	);
-	const lastDate = new Date(new Date().setDate(firstDate.getDate() + 6));
+  const now = new Date(time);
+  const firstDate = new Date(
+    new Date().setDate(
+      now.getDate() - now.getDay() + (now.getDay() === 0 ? -6 : 1)
+    )
+  );
+  const lastDate = new Date(new Date().setDate(firstDate.getDate() + 6));
 
-	return calcDayDiff(firstDate, now, lastDate);
+  return calcDayDiff(firstDate, now, lastDate);
 };
 
 export const calcDay = () => {
-	const start = new Date();
-	start.setHours(0, 0, 0, 0);
+  const start = new Date();
+  start.setHours(0, 0, 0, 0);
 
-	const end = new Date();
-	end.setHours(23, 59, 59, 999);
+  const end = new Date();
+  end.setHours(23, 59, 59, 999);
 
-	const passedDay = Math.abs(new Date() - start);
-	const totalDay = Math.abs(end - start);
+  const passedDay = Math.abs(new Date() - start);
+  const totalDay = Math.abs(end - start);
 
-	return (passedDay / totalDay) * 100;
+  return (passedDay / totalDay) * 100;
 };
 
 export const calcValentine = now => {
-	return calcActualDate(now, '2/14');
+  return calcActualDate(now, '2/14');
 };
 
 export const calc1111 = now => {
-	return calcActualDate(now, '11/11');
+  return calcActualDate(now, '11/11');
+};
+
+export const calcMonthWeekDay = (now, month, weekNo, day) => {
+  const lastYearDate = getActualDate(
+    new Date().getFullYear() - 1,
+    month,
+    weekNo,
+    day
+  );
+  const thisYearDate = getActualDate(
+    new Date().getFullYear(),
+    month,
+    weekNo,
+    day
+  );
+  const nextYearDate = getActualDate(
+    new Date().getFullYear() + 1,
+    month,
+    weekNo,
+    day
+  );
+
+  function getActualDate(year, month, weekNo, day) {
+    const firstDateOfMonth = new Date(`${year}/${month}/1`);
+    const diff = day - firstDateOfMonth.getDay();
+    return new Date(
+      firstDateOfMonth.setDate(
+        firstDateOfMonth.getDate() + diff + (weekNo - 1) * 7
+      )
+    );
+  }
+
+  if (now < thisYearDate) {
+    return calcDayDiff(lastYearDate, now, thisYearDate);
+  }
+
+  return calcDayDiff(thisYearDate, now, nextYearDate);
 };
 
 function calcActualDate(now, theDate) {
-	const lastYearDate = new Date(`${now.getFullYear() - 1}/${theDate}`);
-	const thisYearDate = new Date(`${now.getFullYear()}/${theDate}`);
-	const nextYearDate = new Date(`${now.getFullYear() + 1}/${theDate}`);
+  const lastYearDate = new Date(`${now.getFullYear() - 1}/${theDate}`);
+  const thisYearDate = new Date(`${now.getFullYear()}/${theDate}`);
+  const nextYearDate = new Date(`${now.getFullYear() + 1}/${theDate}`);
 
-	if (now < thisYearDate) {
-		return calcDayDiff(lastYearDate, now, thisYearDate);
-	}
+  if (now < thisYearDate) {
+    return calcDayDiff(lastYearDate, now, thisYearDate);
+  }
 
-	return calcDayDiff(thisYearDate, now, nextYearDate);
+  return calcDayDiff(thisYearDate, now, nextYearDate);
 }
 
 function calcDayDiff(start, cur, end) {
-	start = start.setHours(0, 0, 0, 0);
-	end = end.setHours(23, 59, 59, 999);
+  start = start.setHours(0, 0, 0, 0);
+  end = end.setHours(23, 59, 59, 999);
 
-	const diffTime = Math.abs(cur - start);
-	const totalDiffInMilliseconds = Math.abs(end - start);
+  const diffTime = Math.abs(cur - start);
+  const totalDiffInMilliseconds = Math.abs(end - start);
 
-	return (diffTime / totalDiffInMilliseconds) * 100;
+  return (diffTime / totalDiffInMilliseconds) * 100;
 }
